af44a24d78fca967d2e30bf52e1982dad5d918c7
Hamza Aslan <hamzaaslan@hotmail.com>
2018-11-27 21:33:00 +0300

    Add ExchangeOperastion

--- src/Menu.java
+++ src/Menu.java
@@ -14,36 +14,7 @@ class Menu {
 
     Menu() {
 
-
-        Person person = new Person("1234", "Elif", "Aslan", parseDateFromString("12/12/1900"));
-        person.setId(personList.size()+1);
-        personList.add(person);
-        Person person2 = new Person("12345", "Yusuf", "Aslan", parseDateFromString("12/12/1901"));
-        person2.setId(personList.size()+1);
-        personList.add(person2);
-        Person person3 = new Person("123456", "Betül", "Aslan", parseDateFromString("12/12/1902"));
-        person3.setId(personList.size()+1);
-        personList.add(person3);
-        Person person4 = new Person("1234567", "Hamza", "Aslan", parseDateFromString("12/12/1903"));
-        person4.setId(personList.size()+1);
-        personList.add(person4);
-        Person person5 = new Person("12345678", "Zeynep", "Aslan", parseDateFromString("12/12/1904"));
-        person5.setId(personList.size()+1);
-        personList.add(person5);
-        Person person6 = new Person("123456789", "Asuman", "Aslan", parseDateFromString("12/12/1905"));
-        person6.setId(personList.size()+1);
-        personList.add(person6);
-
-        Account account = new Account("123456789", "Asuman");
-        account.setId(accountList.size()+1);
-        accountList.add(account);
-        Account account2 = new Account("12345", "Yusuf");
-        account2.setId(accountList.size()+1);
-        accountList.add(account2);
-        Account account3 = new Account("12345678", "Zeynep");
-        account3.setId(accountList.size()+1);
-        accountList.add(account3);
-
+        loadTestDB();
 
         System.out.println("Sistemdeki kayıtlı kişi sayısı: " + personList.size());
         System.out.println("Sistemdeki kayıtlı hesap sayısı: " + accountList.size());
@@ -77,7 +48,7 @@ class Menu {
                     break;
 
                 case 2:
-                    System.out.println("Bakiyeniz:" + bakiye + "TL'dir.");
+                    System.out.print("Bakiyeniz:" + bakiye + "TL'dir.");
                     break;
                 case 3:
                     System.out.println("Ne Kadar Yatıracaksınız?");
@@ -101,7 +72,8 @@ class Menu {
                 case 5:
 
                 case 6:
-
+                    exchangeOperation();
+                    break;
                 case 7:
                     System.out.println("Sistemden Çıkılıyor!");
                     break;
@@ -148,13 +120,13 @@ class Menu {
             return myAccount;
     }
 
-    public Date parseDateFromString(String myStringDate) {
+    private Date parseDateFromString(String myStringDate) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Date date= null;
         try {
             date = sdf.parse(myStringDate);
         } catch (ParseException e) {
-            System.out.println("Lütfen dd/MM/yyyy formatta yeniden giriniz.");
+            System.out.print("Lütfen dd/MM/yyyy formatta yeniden giriniz.");
           //  e.printStackTrace();
         }
         return date ;
@@ -162,11 +134,11 @@ class Menu {
 
     private void createAccount() {
 
-        System.out.println("Hesap Türü Giriniz: ");
+        System.out.print("Hesap Türü Giriniz: ");
         scan.nextLine();
         String type = scan.nextLine();
 
-        System.out.println("Hesap Adı Giriniz: ");
+        System.out.print("Hesap Adı Giriniz: ");
         String name = scan.nextLine();
 
         Account account = new Account(type,name);
@@ -178,21 +150,21 @@ class Menu {
 
     private void createPerson() {
 
-        System.out.println("TC Kimlik No Giriniz: ");
+        System.out.print("TC Kimlik No Giriniz: ");
         scan.nextLine();
         String nationalId = scan.nextLine();
 
-        System.out.println("İsim Giriniz: ");
+        System.out.print("İsim Giriniz: ");
         String firstName = scan.nextLine();
 
-        System.out.println("Soyisim Giriniz: ");
+        System.out.print("Soyisim Giriniz: ");
         String lastName = scan.nextLine();
 
         String bDay;
         Date bDayinDateFormat;
 
         while (true){
-            System.out.println("Doğum Tarihini Giriniz(dd/MM/yyyy): ");
+            System.out.print("Doğum Tarihini Giriniz(dd/MM/yyyy): ");
             bDay = scan.nextLine();
             bDayinDateFormat = parseDateFromString(bDay);
 
@@ -200,10 +172,10 @@ class Menu {
                 break;
         }
 
-        System.out.println("TC Kimlik No: " + nationalId);
-        System.out.println("Adı: " + firstName);
-        System.out.println("Soyadı: " + lastName);
-        System.out.println("Doğum Tarihi: " + bDay);
+        System.out.print("TC Kimlik No: " + nationalId);
+        System.out.print("Adı: " + firstName);
+        System.out.print("Soyadı: " + lastName);
+        System.out.print("Doğum Tarihi: " + bDay);
 
         Person person = new Person(nationalId, firstName, lastName, bDayinDateFormat);
         person.setId(personList.size()+1);
@@ -212,5 +184,69 @@ class Menu {
         personList.add(person);
 
         System.out.println("Kayıtlı kişi sayısı: " + personList.size());
+    }
+
+    private  void exchangeOperation(){
+        Scanner oku = new Scanner(System.in);
+        double dolars, dolara, sonuc, para;
+        dolars=5+(Math.random());
+        dolara=5+(Math.random());
+        int a;
+        System.out.println("Kur Alış: "+ dolara +" Kur Satış: " + dolars );
+        System.out.print("Döviz almak için 1, satmak için 2 yazınız! ");
+        a=oku.nextInt();
+        if(a==1){
+            System.out.print("Ne kadar döviz alacaksınız?");
+            para=oku.nextDouble();
+            sonuc=para*dolara;
+            System.out.println(para+" USD için "+sonuc+" TL gereklidir..");
+
+            System.out.printf(para+" USD için %.2f",sonuc);
+            System.out.print(" TL gereklidir.\n");
+        }
+        else if(a==2){
+            System.out.print("Ne Kadar Döviz Satacaksınız?");
+            para=oku.nextDouble();
+            sonuc=para*dolars;
+            System.out.println(para+" Dolar "+sonuc+" TL yapmaktadır.");
+
+            System.out.printf(para+" Dolar %.2f",sonuc);
+            System.out.print(" TL yapmaktadır.\n");
+        }
+        else
+            System.out.print(" Yanlış işlem seçtiniz!");
+
+    }
+
+    private void loadTestDB(){
+        Person person = new Person("1234", "Elif", "Aslan", parseDateFromString("12/12/1900"));
+        person.setId(personList.size()+1);
+        personList.add(person);
+        Person person2 = new Person("12345", "Yusuf", "Aslan", parseDateFromString("12/12/1901"));
+        person2.setId(personList.size()+1);
+        personList.add(person2);
+        Person person3 = new Person("123456", "Betül", "Aslan", parseDateFromString("12/12/1902"));
+        person3.setId(personList.size()+1);
+        personList.add(person3);
+        Person person4 = new Person("1234567", "Hamza", "Aslan", parseDateFromString("12/12/1903"));
+        person4.setId(personList.size()+1);
+        personList.add(person4);
+        Person person5 = new Person("12345678", "Zeynep", "Aslan", parseDateFromString("12/12/1904"));
+        person5.setId(personList.size()+1);
+        personList.add(person5);
+        Person person6 = new Person("123456789", "Asuman", "Aslan", parseDateFromString("12/12/1905"));
+        person6.setId(personList.size()+1);
+        personList.add(person6);
+
+        Account account = new Account("123456789", "Asuman");
+        account.setId(accountList.size()+1);
+        accountList.add(account);
+        Account account2 = new Account("12345", "Yusuf");
+        account2.setId(accountList.size()+1);
+        accountList.add(account2);
+        Account account3 = new Account("12345678", "Zeynep");
+        account3.setId(accountList.size()+1);
+        accountList.add(account3);
+
     }
 }
